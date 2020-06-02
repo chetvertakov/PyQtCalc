4d4be3c0f60708ec2106f2c03110661ade92fb24
Alexey Chetvertakov <chetvertakov@gmail.com>
2020-06-02 19:42:33 +0300

    res path correct

--- QtTest.py
+++ QtTest.py
@@ -3,13 +3,9 @@ from PyQt5 import uic, QtCore, QtGui, QtWidgets
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
+
 
 class MainWindow(QtWidgets.QMainWindow):
 
@@ -90,7 +86,6 @@ class MainWindow(QtWidgets.QMainWindow):
             16777221: self.BtnEqual,
             16777223: self.BtnClear,
         }
-        print(Button.get(key))
         if (Button.get(key)!=None):
             Button.get(key).click()
 
